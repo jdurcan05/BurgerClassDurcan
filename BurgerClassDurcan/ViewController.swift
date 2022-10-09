ad1c71a64eb25129e133aa2c158e6fe2f4b9b322
James Durcan <jdurcan05@gmail.com>
2022-10-09 11:35:20 -0500

    Initial Commit

---
+++ BurgerClassDurcan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerClassDurcan
+//
+//  Created by JAMES DURCAN on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
